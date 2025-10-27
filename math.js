aee31c3b69589a2503e6c0fc6e2d3c5a109e2141
habib4445 <0124habibur@gmail.com>
2025-10-27 17:29:12 +0600

    mth.js

---
+++ math.js
@@ -0,0 +1,10 @@
+ const min=Math.min(45,776,66,4.34,45)
+ console.log(min);
+  const max=Math.max(45,776,66,4.34,45)
+  console.log(max);
+  console.log(Math.round(2.25));
+  console.log(Math.round(2.85));
+  console.log(Math.floor(2.85));
+  console.log(Math.ceil(2.85));
+  console.log(Math.PI);
+  console.log(Math.abs(-45+776));
