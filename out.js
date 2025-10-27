68a4be7dcaf82a8a7d1dd92b34b7c3c63ece9110
habib4445 <0124habibur@gmail.com>
2025-10-27 19:26:05 +0600

    add

---
+++ out.js
@@ -0,0 +1,10 @@
+function getmax(num1 ,num2){
+    if(num1>num2){
+        return num2;
+    }
+    else{
+        return num2;
+    }
+}
+const max= getmax(12,55);
+console.log('max of two is:',max);
